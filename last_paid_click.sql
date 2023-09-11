169ca4dc0e5e0c70d1b35016c63eaac4796864fa
Мария <128679585+mariapolyakova888@users.noreply.github.com>
2023-09-11 18:00:06 +0700

    Add 1 updated file (step 2)

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,33 +1,28 @@
---Шаг 2. Запрос для витрины по модели атрибуции лидов Last Paid Click:
-with last_touch as (
-    select distinct on (visitor_id)
-        visitor_id,
-        max(visit_date) as visit_date,
-        source,
-        medium,
-        campaign
-    from sessions
-    where
-        medium != 'organic'
-    group by visitor_id, visit_date, source, medium, campaign
-    order by visitor_id, visit_date desc
+--Шаг 2. Построим витрину для модели атрибуции Last Paid Click
+
+with last_paid_click as (
+    select distinct on (s.visitor_id)
+        s.visitor_id,
+        s.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id
+    from sessions as s
+    left join leads as l on s.visitor_id = l.visitor_id
+    where s.medium != 'organic'
+    order by s.visitor_id asc, s.visit_date desc
 )
 
-select
-    last_touch.visitor_id,
-    last_touch.visit_date,
-    last_touch.source as utm_source,
-    last_touch.medium as utm_medium,
-    last_touch.campaign as utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-from last_touch
-left join leads as l
-    on
-        last_touch.visitor_id = l.visitor_id
+select *
+from last_paid_click
 order by
-    l.amount desc nulls last,
-    last_touch.visit_date, utm_source, utm_medium, utm_campaign asc;
+    amount desc nulls last,
+    visit_date asc,
+    utm_source asc,
+    utm_medium asc,
+    utm_campaign asc;

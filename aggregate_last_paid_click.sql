d3c2b3eece1587aceaa97067698bc4527191f77a
Мария <128679585+mariapolyakova888@users.noreply.github.com>
2023-09-12 14:02:18 +0700

    Add 2 updated files (step 3)

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,65 +1,126 @@
---Шаг 3. Построение витрины - расчет расходов на рекламу по модели атрибуции Last Paid Click:
-with tab as (
+-- Шаг 3. Построим витрину по расчету расходов на рекламу
+  
+with t as (
+    select distinct on (s.visitor_id)
+        s.visitor_id,
+        s.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id
+    from sessions as s
+    left join leads as l
+        on
+            s.visitor_id = l.visitor_id
+            and s.visit_date <= l.created_at
+    where s.medium != 'organic'
+    order by s.visitor_id asc, s.visit_date desc
+),
+
+last_paid_click as (
+    select *
+    from t
+    order by
+        amount desc nulls last,
+        visit_date asc,
+        utm_source asc,
+        utm_medium asc,
+        utm_campaign asc
+),
+
+last_paid_click_revenue as (
     select
-        date_trunc('day', campaign_date) as campaign_date,
+        date_trunc('day', visit_date)::date as visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        count(visitor_id) as visitors_count,
+        count(lead_id) as leads_count,
+        count(
+            case
+                when
+                    closing_reason = 'Успешная продажа' or status_id = 142
+                    then lead_id
+            end
+        ) as purchases_count,
+        sum(
+            case
+                when
+                    closing_reason = 'Успешная продажа' or status_id = 142
+                    then amount
+            end
+        ) as revenue
+    from last_paid_click
+    group by
+        date_trunc('day', visit_date)::date,
+        utm_source,
+        utm_medium,
+        utm_campaign
+    order by
+        revenue desc nulls last,
+        visit_date asc,
+        visitors_count desc,
+        utm_source asc,
+        utm_medium asc,
+        utm_campaign asc
+),
+
+vk as (
+    select
+        date_trunc('day', campaign_date)::date as campaign_date,
         utm_source,
         utm_medium,
         utm_campaign,
         sum(daily_spent) as daily_spent
     from vk_ads
     where utm_medium != 'organic'
-    group by 1, 2, 3, 4
-    order by 1, 3, 4 asc
+    group by
+        date_trunc('day', campaign_date)::date,
+        utm_source,
+        utm_medium,
+        utm_campaign
 ),
 
-tab1 as (
+ya as (
     select
-        date_trunc('day', campaign_date) as campaign_date,
+        date_trunc('day', campaign_date)::date as campaign_date,
         utm_source,
         utm_medium,
         utm_campaign,
         sum(daily_spent) as daily_spent
     from ya_ads
     where utm_medium != 'organic'
-    group by 1, 2, 3, 4
-    order by 1, 3, 4 asc
+    group by
+        date_trunc('day', campaign_date)::date,
+        utm_source,
+        utm_medium,
+        utm_campaign
 )
 
 select
-    date_trunc('day', lpc.visit_date) as visit_date,
-    lpc.utm_source,
-    lpc.utm_medium,
-    lpc.utm_campaign,
-    count(lpc.visitor_id) as visitors_count,
-    case
-	    when tab.daily_spent is not null then tab.daily_spent
-	    when tab1.daily_spent is not null then tab1.daily_spent
-	    else 0
-	end as total_cost,
-    count(lpc.lead_id) as leads_count,
-    count(lpc.status_id) as purchases_count,
-    sum(lpc.amount) as revenue
-from last_paid_click_mplkv as lpc --в базе данных marketingdb создано представление last_paid_click_mplkv
-left join tab
+    lpcr.visit_date,
+    lpcr.utm_source,
+    lpcr.utm_medium,
+    lpcr.utm_campaign,
+    lpcr.visitors_count,
+    lpcr.leads_count,
+    lpcr.purchases_count,
+    lpcr.revenue,
+    coalesce(vk.daily_spent, ya.daily_spent, 0) as total_cost
+from last_paid_click_revenue as lpcr
+left join vk
     on
-        date_trunc('day', lpc.visit_date) = tab.campaign_date
-        and lpc.utm_source = tab.utm_source
-        and lpc.utm_medium = tab.utm_medium
-        and lpc.utm_campaign = tab.utm_campaign
-left join tab1
+        lpcr.visit_date = vk.campaign_date
+        and lpcr.utm_source = vk.utm_source
+        and lpcr.utm_medium = vk.utm_medium
+        and lpcr.utm_campaign = vk.utm_campaign
+left join ya
     on
-        date_trunc('day', lpc.visit_date) = tab1.campaign_date
-        and lpc.utm_source = tab1.utm_source
-        and lpc.utm_medium = tab1.utm_medium
-        and lpc.utm_campaign = tab1.utm_campaign
-where lpc.status_id = 142
-group by
-    visit_date,
-    lpc.utm_source,
-    lpc.utm_medium,
-    lpc.utm_campaign,
-    tab.daily_spent,
-    tab1.daily_spent
-order by
-    revenue desc nulls last, visitors_count desc,
-    visit_date, lpc.utm_source, lpc.utm_medium, lpc.utm_campaign asc;
+        lpcr.visit_date = ya.campaign_date
+        and lpcr.utm_source = ya.utm_source
+        and lpcr.utm_medium = ya.utm_medium
+        and lpcr.utm_campaign = ya.utm_campaign;
